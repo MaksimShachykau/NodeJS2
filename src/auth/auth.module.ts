23570e8185ed3efe1d0513a2ec13c7b3530cbbd2
Maksim shachykau <50798581+MaksimShachykau@users.noreply.github.com>
2023-12-21 12:36:54 +0100

    B-13: add tests fot auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 
-import { getJWTConfig } from 'src/configs/jwt.config';
+import { getJWTConfig } from '../configs/jwt.config';
 import { AuthController } from './auth.controller';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
-import { JWTStrategy } from 'src/auth/strategies/jwt.strategy';
+import { JWTStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
